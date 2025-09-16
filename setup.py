8ad24082cae7644c93e75f7c535cb74d194453a4
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2025-09-16 13:16:44 -0400

    subfolder src

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sac',
+    version='0.1.0',
+    description='State Abstraction & Capacity',
+    author='Sebastian Lee, Manu Halvagal',
+    author_email='sebastianlee.1997@yahoo.co.uk',
+    packages=find_packages(), 
+    install_requires=[],
+)
