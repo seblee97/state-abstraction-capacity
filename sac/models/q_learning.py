8b87ebf7110fe0373e3bca3a1a263eb18b4d238c
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2025-10-05 15:03:32 -0400

    init base class

--- sac/models/q_learning.py
+++ sac/models/q_learning.py
@@ -1,18 +1,20 @@
-from sac.models import base 
+import os
+from sac.models import base
 
 import numpy as np
 
+
 class QLearning(base.BaseModel):
-    
+
     def __init__(
-            self, 
-            action_space, 
-            state_space, 
-            learning_rate: float, 
-            discount_factor: float, 
-            exploration_rate: float, 
-            exploration_decay: float
-        ):
+        self,
+        action_space,
+        state_space,
+        learning_rate: float,
+        discount_factor: float,
+        exploration_rate: float,
+        exploration_decay: float,
+    ):
 
         self._action_space = action_space
         self._state_space = state_space
@@ -26,7 +28,11 @@ class QLearning(base.BaseModel):
         self._exploration_decay = exploration_decay
 
         # zero-initialize for now
-        self._state_action_values = np.zeros((len(self._state_space), len(self._action_space)))
+        self._state_action_values = np.zeros(
+            (len(self._state_space), len(self._action_space))
+        )
+
+        super().__init__()
 
     def select_action(self, state):
         if np.random.rand() < self._exploration_rate:
@@ -34,7 +40,7 @@ class QLearning(base.BaseModel):
         else:
             action = self.select_greedy_action(state)
         return action
-    
+
     def select_greedy_action(self, state):
         state_id = self._state_id_mapping[state]
         action = np.argmax(self._state_action_values[state_id])
@@ -59,15 +65,16 @@ class QLearning(base.BaseModel):
 
         initial_value = self._state_action_values[state_id][action]
         new_sate_values = self._state_action_values[new_state_id]
-        
+
         td_error = reward + discount * np.max(new_sate_values) - initial_value
 
         updated_value = initial_value + self._learning_rate * td_error
         self._state_action_values[state_id][action] = updated_value
 
-        return {"loss": td_error ** 2}
+        return {"loss": td_error**2}
 
-
-    def save_model(self, path):
+    def save_model(self, path: str, episode: int) -> None:
         # Save the Q-table to a file
-        pass
+        np.savez(
+            os.path.join(path, f"q_table_{episode}.npz"), self._state_action_values
+        )
