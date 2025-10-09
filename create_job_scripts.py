84955add00f04308030aeaa279cc8fd7dc971a20
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2025-10-09 18:20:27 -0400

    change output dirs, change hyperparam names

--- create_job_scripts.py
+++ create_job_scripts.py
@@ -2,6 +2,7 @@ import os
 import itertools
 import subprocess
 import time
+import numpy as np
 
 lrs = [0.0001, 0.0003, 0.0005]
 batch_sizes = [32, 128, 512]
@@ -22,22 +23,35 @@ job_script_template = """#!/bin/bash
 #SBATCH --cpus-per-task 4
 #SBATCH --mem 4G
 #SBATCH --time=0-05:00:00
-#SBATCH --output=/mnt/home/slee1/ceph/unc_mattar_results/2025-10-08-16-41-40/dyna_0/0/output.txt
-#SBATCH --error=/mnt/home/slee1/ceph/unc_mattar_results/2025-10-08-16-41-40/dyna_0/0/error.txt
-source /mnt/home/slee1/venvs/sac/bin/activate\n
 """
+
+sweep_config = {}
 
 for idx, (lr, batch_size, buffer_size, kl, epoch, entropy) in enumerate(
     itertools.product(lrs, batch_sizes, buffer_sizes, kls, epochs, entropies)
 ):
+    sweep_config[idx] = {
+        "lr": lr,
+        "batch_size": batch_size,
+        "buffer_size": buffer_size,
+        "kl": kl,
+        "epoch": epoch,
+        "entropy": entropy,
+    }
     # create subdirectory for each job script
-    os.makedirs(f"job_scripts/{timestamp}/job_{idx}", exist_ok=True)
-    with open(f"job_scripts/{timestamp}/job_{idx}/job_{idx}", "w") as f:
+    job_dir = os.path.join(f"job_scripts/{timestamp}", f"job_{idx}")
+    os.makedirs(job_dir, exist_ok=True)
+    with open(f"{job_dir}/job_{idx}", "w") as f:
         f.write(job_script_template)
-        f.write(f"python /mnt/home/slee1/state-abstraction-capacity/sac/run.py -m ppo -lr {lr} -bs {batch_size} -buf {buffer_size} -kl {kl} -ep {epoch} -ent {entropy}\n")
+        f.write(f"#SBATCH --output={job_dir}/output.txt\n")
+        f.write(f"#SBATCH --error={job_dir}/error.txt\n")
+        f.write("source /mnt/home/slee1/venvs/sac/bin/activate\n")
+        f.write(f"python /mnt/home/slee1/state-abstraction-capacity/sac/run.py -m ppo -lr {lr} -bs {batch_size} -rbs {buffer_size} -kl {kl} -ue {epoch} -ec {entropy}\n")
 
     # make the script executable
     os.chmod(f"job_scripts/{timestamp}/job_{idx}/job_{idx}", 0o755)
 
     # run the command to submit the job
     subprocess.call(f"sbatch job_scripts/{timestamp}/job_{idx}/job_{idx}", shell=True)
+
+np.save(f"job_scripts/{timestamp}/sweep_config.npy", sweep_config)
