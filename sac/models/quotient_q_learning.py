9cf4f809cfbfdbc36ca01a8a2c3aff0e42e17b41
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2025-12-10 17:07:06 -0500

    state id

--- sac/models/quotient_q_learning.py
+++ sac/models/quotient_q_learning.py
@@ -9,6 +9,7 @@ class QuotientQLearning(base.BaseModel):
 
     def __init__(
         self,
+        state_space,
         state_blocks,
         state_label,
         sa_label,
@@ -18,6 +19,10 @@ class QuotientQLearning(base.BaseModel):
         exploration_rate: float,
         exploration_decay: float,
     ):
+
+        self._state_space = state_space
+
+        self._state_id_mapping = {state: i for i, state in enumerate(self._state_space)}
 
         self._state_label = state_label
         self._sa_label = sa_label
@@ -37,19 +42,21 @@ class QuotientQLearning(base.BaseModel):
         super().__init__()
 
     def select_action(self, state):
-        b = self._state_label[state]
+        state_id = self._state_id_mapping[state]
+        b = self._state_label[state_id]
         if np.random.rand() < self._exploration_rate:
             valid_is = np.flatnonzero(self._valid_mask[b])
-            action = self._to_base_action(int(np.random.choice(valid_is)))
+            action = self._to_base_action(int(np.random.choice(valid_is)), state_id)
         else:
             action = self.select_greedy_action(state)
         return action
 
     def select_greedy_action(self, state):
-        b = self._state_label[state]
+        state_id = self._state_id_mapping[state]
+        b = self._state_label[state_id]
         q_row = np.where(self._valid_mask[b], self._Q_tilde[b], -np.inf)
         i = int(np.argmax(q_row))
-        return self._to_base_action(b, i, state)
+        return self._to_base_action(i, state_id)
 
     def step(
         self,
@@ -60,8 +67,11 @@ class QuotientQLearning(base.BaseModel):
         active: bool,
     ):
         # (s,a) -> (b,i)
-        b = self._state_label[state]
-        sa_cls = self._sa_label[state, action]
+        state_id = self._state_id_mapping[state]
+        new_state_id = self._state_id_mapping[new_state]
+
+        b = self._state_label[state_id]
+        sa_cls = self._sa_label[state_id, action]
         i = self._action_index_per_block[b][sa_cls]
 
         if active:
@@ -70,10 +80,11 @@ class QuotientQLearning(base.BaseModel):
             discount = 0
 
         # target uses next abstract state b'
-        b_next = self._state_label[new_state]
+        b_next = self._state_label[new_state_id]
         q_next = np.where(self._valid_mask[b_next], self._Q_tilde[b_next], -np.inf)
         td_target = reward + discount * np.max(q_next)
         td_error = td_target - self._Q_tilde[b, i]
+
         self._Q_tilde[b, i] += self._learning_rate * td_error
 
         return {"loss": td_error**2}
